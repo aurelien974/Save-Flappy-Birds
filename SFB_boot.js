53dcbbd141b8a38390343e60f5092b31b8600308
aurelien974 <aurelien.picolet@gmail.com>
2014-04-19 09:39:20 +0400

    commit for scale manager
    align canvas added
    scale mode in commentary

--- SFB_boot.js
+++ SFB_boot.js
@@ -13,6 +13,13 @@ SFB.Boot.prototype = {
 
 	preload: function () {
 		
+		this.game.stage.scale.pageAlignHorizontally = true;
+		this.game.stage.scale.pageAlignVertically = true;
+		// this.game.stage.scale.maxWidth = 320;	
+		// this.game.stage.scale.maxHeight = 480;	
+		// this.game.stage.scaleMode = Phaser.StageScaleMode.SHOW_ALL;
+		this.game.stage.scale.refresh();
+		
 		this.game.stage.backgroundColor = '#000000';
 		this.game.load.image('loadingBar', 'loading_bar_1.png');
 		
